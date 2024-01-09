1b4790d6be17d4d8f064eb72ee6ba9f4d8adaab1
Dattatray Gund <dattatray.laxmangund@virtana.com>
2024-01-10 00:00:47 +0530

    Updates

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -3,9 +3,15 @@
 import com.example.constants.ApplicationConstants
 
 def call(String name = null) {
-    def resource = libraryResource 'config.properties'
-    echo "resource: $resource"
-    //def props = readProperties text: resource
-    def configname = resource[ApplicationConstants.NAME]
-    echo "Greeting: $configname"
+    script {
+        def resource = libraryResource 'config.properties'
+        echo "resource: $resource"
+
+        def properties = new Properties()
+        properties.load(new StringReader(resource))
+
+        //def props = readProperties text: resource
+        def configname = properties.getProperty(ApplicationConstants.NAME)
+        echo "Greeting: $configname"
+    }
 }

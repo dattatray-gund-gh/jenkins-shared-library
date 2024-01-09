8392d13ffd7a1ced36aaa7ea239c911c6220701c
Dattatray Gund <dattatray.laxmangund@virtana.com>
2024-01-10 00:14:26 +0530

    Updates

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -6,11 +6,8 @@ import com.example.constants.ApplicationConstants
 def call(String name = null) {
     script {
 
-
-
-        def config = libraryResource('config.properties')
-        def systemConfig = SystemConfig.getInstance(config)
-        def updatedName = name ?: systemConfig.getProperty(ApplicationConstants.NAME)
+        def config = libraryResource('config.properties') as Properties
+        def updatedName = name ?: config.getProperty(ApplicationConstants.NAME)
         Utils utils = new Utils()
         echo "Greeting: ${utils.sayHello(updatedName)}"
     }

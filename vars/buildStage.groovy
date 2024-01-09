036cbfa4c1bd14b9bdad02ed33e2abca0f4add12
Dattatray Gund <dattatray.laxmangund@virtana.com>
2024-01-09 23:59:02 +0530

    Updates

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -6,6 +6,6 @@ def call(String name = null) {
     def resource = libraryResource 'config.properties'
     echo "resource: $resource"
     //def props = readProperties text: resource
-//    def configname = props[ApplicationConstants.NAME]
+    def configname = resource[ApplicationConstants.NAME]
     echo "Greeting: $configname"
 }

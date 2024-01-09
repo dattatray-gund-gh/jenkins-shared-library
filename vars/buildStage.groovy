cd257c05b5d0be69725a8a955447e70c949f55ad
Dattatray Gund <dattatray.laxmangund@virtana.com>
2024-01-09 23:37:36 +0530

    Updates

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -5,9 +5,15 @@ import com.example.constants.ApplicationConstants
 
 def call(String name = null) {
     script {
-        def config = SystemConfig.loadConfig()
+       // def config = SystemConfig.loadConfig()
 //        def configValue = config.getProperty(ApplicationConstants.NAME)
 //        def greeting = new Utils().sayHello(configValue)
+
+        def properties = new Properties()
+        def configFile = new File(this.getClass().getResource('/resources/config.properties').toURI())
+        properties.load(configFile.newReader())
+
+        def config = properties.getProperty(ApplicationConstants.NAME)
         echo "Greeting: $config"
     }
 }

1d0768a0fb0e2312575ee1d9205b2d6e6354d811
Dattatray Gund <dattatray.laxmangund@virtana.com>
2024-01-09 23:11:23 +0530

    Updates

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -5,7 +5,7 @@ import com.example.constants.ApplicationConstants
 
 def call(String name = null) {
     script {
-        def configPath = "${JENKINS_HOME}/jobs/${JOB_NAME}/workspace/my-shared-library/resources/config.properties"
+        def configPath = "${JENKINS_HOME}/jobs/${JOB_NAME}/workspace/jenkins-shared-library/resources/config.properties"
         echo "Config Path: $configPath"
         def config = SystemConfig.loadConfig(configPath)
         def configValue = config.getProperty(ApplicationConstants.NAME)

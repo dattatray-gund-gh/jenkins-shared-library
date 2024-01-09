0cecd4dbfa7e29d8f63ac4a6e2f187b6435ceb44
Dattatray Gund <dattatray.laxmangund@virtana.com>
2024-01-09 23:46:25 +0530

    Updates

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -3,10 +3,5 @@
 import com.example.constants.ApplicationConstants
 
 def call(String name = null) {
-    // Load config.properties file from resources folder and read the name property
-    def properties = new Properties()
-    InputStream stream = getClass().getClassLoader().getResourceAsStream("resources/config.properties")
-    properties.load(stream)
-    def config = properties.getProperty(ApplicationConstants.NAME)
-    echo "Greeting: $config"
+    echo "Greeting: $libraryResource"
 }

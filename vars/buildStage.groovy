697f9d07c7a4527f6af63d515c59a6830e07d750
Dattatray Gund <dattatray.laxmangund@virtana.com>
2024-01-09 23:14:50 +0530

    Updates

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -5,7 +5,7 @@ import com.example.constants.ApplicationConstants
 
 def call(String name = null) {
     script {
-        def libPath = "${JENKINS_HOME}/jobs/${JOB_NAME}"
+        def libPath = "${JENKINS_HOME}"
         sh "ls -l ${libPath}"
 //        def configPath = "${libPath}/resources/config.properties"
 //        echo "Config Path: $configPath"

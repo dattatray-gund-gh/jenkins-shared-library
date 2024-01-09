776b9a1a119d9529ea4573b9dcac02dcd0a07e71
Dattatray Gund <dattatray.laxmangund@virtana.com>
2024-01-09 23:07:28 +0530

    Updates

--- src/com/example/SystemConfig.groovy
+++ src/com/example/SystemConfig.groovy
@@ -4,8 +4,9 @@ class SystemConfig {
     static Properties loadConfig() {
         def properties = new Properties()
         try {
-            def configFile = new File(this.class.protectionDomain.codeSource.location.path).parent + "/resources/config.properties"
-            properties.load(new FileInputStream(configFile))
+            //properties.load(new FileInputStream("${JENKINS_HOME}/jobs/${JOB_NAME}/workspace/my-shared-library/resources/config.properties"))
+//            def configFile = new File(this.class.protectionDomain.codeSource.location.path).parent + "/resources/config.properties"
+            properties.load(new FileInputStream(new FileInputStream("${JENKINS_HOME}/jobs/${JOB_NAME}/workspace/jenkins-shared-library/resources/config.properties"))
         } catch (IOException e) {
             throw new RuntimeException("Failed to load configuration properties.", e)
         }
